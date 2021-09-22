ee9306bb5a94b75cdfa80820b6e8c893be948c1e
Tomer Aberbach <tomeraberbach@gmail.com>
2021-09-21 20:03:40 -0400

    style: relint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import fs from 'fs'
 import { sep, resolve } from 'path'
 
@@ -5,7 +21,7 @@ const fa = fs.promises
 
 const normalizeOptions = ({
   resolve = false,
-  isExcludedDir = () => false
+  isExcludedDir = () => false,
 } = {}) => ({ resolve, isExcludedDir })
 
 const normalizeDirname = (dirname, options) => {
@@ -46,7 +62,7 @@ export const getAllFilesSync = (filename, options) => {
         }
       })(normalizeDirname(filename, options))
     },
-    toArray: () => [...files]
+    toArray: () => [...files],
   }
 
   return files
@@ -83,7 +99,7 @@ function createNotifier() {
     },
     onResolved() {
       return notified
-    }
+    },
   }
 }
 
@@ -157,7 +173,7 @@ export const getAllFiles = (filename, options) => {
       }
 
       return filenames
-    }
+    },
   }
 
   return files
