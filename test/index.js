197825c9c2b8b0b237f3a0fac3043c11f50336b7
rerades <rerades@siete3.com>
2021-12-30 09:59:36 +0100

    removing semi

--- test/index.js
+++ test/index.js
@@ -14,49 +14,49 @@
  * limitations under the License.
  */
 
-import fs from 'fs';
-import test from 'ava';
-import { getAllFiles, getAllFilesSync } from '../src/index.js';
+import fs from 'fs'
+import test from 'ava'
+import { getAllFiles, getAllFilesSync } from '../src/index.js'
 
-const fixtures = `./test/fixtures`;
+const fixtures = `./test/fixtures`
 
-const isInList = list => name => list.indexOf(name) > -1;
-const excludedDirs = [`./test/fixtures/blah/unreal/woah/`];
+const isInList = list => name => list.indexOf(name) > -1
+const excludedDirs = [`./test/fixtures/blah/unreal/woah/`]
 
 const options = {
   isExcludedDir: isInList(excludedDirs),
-};
+}
 
 test(`sync finds 6 files`, t => {
-  let count = 0;
+  let count = 0
 
   for (const filename of getAllFilesSync(fixtures)) {
-    t.assert(fs.existsSync(filename));
-    count++;
+    t.assert(fs.existsSync(filename))
+    count++
   }
 
-  t.is(count, 6);
-});
+  t.is(count, 6)
+})
 
 test(`sync array finds 6 files`, t => {
-  t.is(getAllFilesSync(fixtures).toArray().length, 6);
-});
+  t.is(getAllFilesSync(fixtures).toArray().length, 6)
+})
 
 test(`async finds 6 files`, async t => {
-  let count = 0;
+  let count = 0
 
   for await (const filename of getAllFiles(fixtures)) {
-    await fs.promises.access(filename);
-    count++;
+    await fs.promises.access(filename)
+    count++
   }
 
-  t.is(count, 6);
-});
+  t.is(count, 6)
+})
 
 test(`async array finds 6 files`, async t => {
-  t.is((await getAllFiles(fixtures).toArray()).length, 6);
-});
+  t.is((await getAllFiles(fixtures).toArray()).length, 6)
+})
 
 test(`async array finds 4 files with excluding options object`, async t => {
-  t.is((await getAllFiles(fixtures, options).toArray()).length, 4);
-});
+  t.is((await getAllFiles(fixtures, options).toArray()).length, 4)
+})
